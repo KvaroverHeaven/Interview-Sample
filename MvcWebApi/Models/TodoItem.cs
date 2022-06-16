2fd1a17761c750866aa3b48f9d4272a516063e01
kvarnoel <skyrainfit@gmail.com>
2022-06-16 19:43:13 +0800

    [Model] Add TodoItem models

---
+++ MvcWebApi/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace MvcWebApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
